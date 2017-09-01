299cb09f07e4ddb40c38a9e707420eae26a93b53
DavidFBakker <dbakker@arrayasolutions.com>
2017-09-01 15:25:03 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyConfig;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var vp = Config.GetValuePair("LogName");
+
+            if (vp == null)
+                Config.SetValuePair(new ValuePair {Name = "LogName", Value = "Log.txt"});
+
+            vp = Config.GetValuePair("LogName");
+        }
+    }
+}
---
+++ MyConfig/Configs.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace MyConfig
+{
+    // Super cool. I now have a class where i can store name=values in.
+    // ValuePairs:List<ValuePair> Basically it turns your ValuePair class into a List 
+    public class ValuePairs : List<ValuePair>
+    {
+    }
+
+    public class ValuePair
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public static class Config
+    {
+        public static ValuePair GetValuePair(string name)
+        {
+            var vp = ValuePairs();
+            var value = vp.SingleOrDefault(a => a.Name == name);
+            return value;
+        }
+
+        public static void SetValuePair(ValuePair valuePair)
+        {
+            var vp = ValuePairs();
+            var value = vp.SingleOrDefault(a => a.Name == valuePair.Name);
+
+            if (value != null) //Exists lets just update the value
+                value.Name = valuePair.Name;
+            else
+                vp.Add(valuePair);
+
+            var towrite = JsonConvert.SerializeObject(vp);
+
+            File.WriteAllText("configFile.json", towrite);
+        }
+
+        private static ValuePairs ValuePairs()
+        {
+            var input = File.ReadAllText("configFile.json");
+            var valuePairs = JsonConvert.DeserializeObject<ValuePairs>(input);
+            return valuePairs;
+        }
+    }
+}
